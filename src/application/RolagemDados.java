6b573ea9d58dce1025bf99574ec3ee9e483d0cf5
Jorge Henrique <jh_cartaxo@hotmail.com>
2025-07-21 18:04:14 -0300

    Programa finalizado

--- src/application/RolagemDados.java
+++ src/application/RolagemDados.java
@@ -14,19 +14,31 @@ public class RolagemDados {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
 
+        intro();
+        System.out.println("Role o dado ou digite 'parar' para sair:");
         while (true) {
             String rolagem = sc.nextLine();
-
             if (rolagem.equalsIgnoreCase("parar")) {
+                System.out.println("Rolls finalizados. Até mais!!");
                 break;
             }
-
-            Resultado resultado = interpretacaoDosDados(rolagem, random);
-
-            System.out.println("Resultado: " + resultado.getCalc());
-            System.out.println();
+            try {
+                Resultado resultado = interpretacaoDosDados(rolagem, random);
+                System.out.println("Resultado: " + resultado.getCalc());
+                System.out.println();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Formato inválido! Modo de usar: 2d6+4 ou 1d20+2+5\n");
+                System.out.println("Tente novamente:");
+            }
         }
-
         sc.close();
+    }
+    private static void intro() {
+        System.out.println("----------------------------------");
+        System.out.println("🎲 Bem vindo ao DiceRolls 🎲");
+        System.out.println("Como usar: informe a quantidade, o dado e o bônus.");
+        System.out.println("Exemplo: 1d20 ou 1d20+2+5");
+        System.out.println("Digite 'parar' para encerrar o programa!");
+        System.out.println("----------------------------------");
     }
 }
